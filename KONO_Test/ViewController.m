901e7c973424b183d03b1f1ded704eafcf7ec477
CI-Starly.Chen <starly.chen@cloud-interactive.com>
2019-09-25 09:41:48 +0800

    The First Commit

--- KONO_Test/ViewController.m
+++ KONO_Test/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    int number;
+}
 
 @end
 
@@ -16,8 +18,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+   
+    [self tryDataRace];
+
+   
 }
+
+-(void) deadLock1 {
+    NSLog(@"1");
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        NSLog(@"2");
+    });
+}
+
+-(void) deadLock2 {
+dispatch_async(dispatch_get_global_queue(0, 0), ^{
+   NSLog(@"1");
+   //回到主线程发现死循环后面就没法执行了
+   dispatch_sync(dispatch_get_main_queue(), ^{
+       NSLog(@"2");
+   });
+   NSLog(@"3");
+});
+NSLog(@"4");
+//死循环
+    while (1) {
+       //
+    }
+}
+
+
+-(void) deadLockSolution1 {
+    NSLog(@"1");
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"2");
+    });
+}
+
+-(void) deadLockSolution2 {
+    NSLog(@"1");
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+         NSLog(@"2");
+    });
+}
+
+
+-(void) tryDataRace {
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+         for (int i = 0 ; i < 10; i++) {
+             self->number += 1;
+             NSLog(@"%d", self->number);
+           }
+    });
+   
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        for (int i = 0 ; i < 10; i++) {
+            self->number -= 1;
+            NSLog(@"%d", self->number);
+        }
+    });
+}
+
+
 
 
 @end
